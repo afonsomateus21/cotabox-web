290e50c6626227a9fdfec6e71a3714560b136be1
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-02-20 14:54:37 -0300

    fix: corrigindo userSchema para permitir apenas valores entre 1 e 100

--- src/components/Form/schema.ts
+++ src/components/Form/schema.ts
@@ -1,7 +1,16 @@
 import { number, object, string } from "yup";
 
 export const userSchema = object({
-  firstName: string().required().matches(/^[a-zA-ZÀ-ÖØ-öø-ÿ\s]*$/, "First name must contain only text"),
-  lastName: string().required().matches(/^[a-zA-ZÀ-ÖØ-öø-ÿ\s]*$/, "Last name must contain only text"),
-  participation: number().typeError("Participation must be a number").positive("Participation must me a positive number").required()
+  firstName: string()
+    .required()
+    .matches(/^[a-zA-ZÀ-ÖØ-öø-ÿ\s]*$/, "First name must contain only text"),
+  lastName: string()
+    .required()
+    .matches(/^[a-zA-ZÀ-ÖØ-öø-ÿ\s]*$/, "Last name must contain only text"),
+  participation: number()
+    .typeError("Participation must be a number")
+    .positive("Participation must me a positive number")
+    .min(1)
+    .max(100)
+    .required()
 })
